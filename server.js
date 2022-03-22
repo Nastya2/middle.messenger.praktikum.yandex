a90f23a2d8ff83062f801adaf84a31cc6dd0a579
Ваше Имя <you@example.com>
2022-03-22 19:53:57 +0300

    sprint_1

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = 3000;
-const root = "public";
 
 
-app.use(express.static(`${root}`));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
